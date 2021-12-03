f6e413b0c7f6e4b9ba58c08bf72574b88e94c27e
Muhammad Fahad <fahad.ansari88@yahoo.com>
2021-12-04 01:41:02 +0500

    changes!!

--- Web/fund-project/src/app/components/dashboard/header/header.component.ts
+++ Web/fund-project/src/app/components/dashboard/header/header.component.ts
@@ -8,15 +8,27 @@ import { APIService } from 'src/app/services/api.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  role: any = '';
   constructor(private router: Router, private apiService: APIService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.role = sessionStorage.getItem('role');
+  }
 
   Logout() {
     this.apiService.logout().subscribe((result: any) => {
       sessionStorage.clear();
       this.router.navigate(['login']);
     });
+  }
+
+  displayName(name: string) {
+    let str = '';
+    if (name) {
+      let role = name.charAt(0).toUpperCase() + name.slice(1);
+      str = 'Fund ' + role;
+    }
+    return str;
   }
 
   Reload() {
