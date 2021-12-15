e6ac5753484d3235431b392d6c19432391ca09f7
Muhammad Fahad <fahad.ansari88@yahoo.com>
2021-12-16 01:55:05 +0500

    backend commit

--- API/core/models.py
+++ API/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.base import Model
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
@@ -12,24 +11,24 @@ USER_DESIGNATION=(
 )
 
 FUND_TYPE=(
-    (0,'Regulated'),
-    (1,'Unregulated')
+    (0,'regulated'),
+    (1,'unregulated')
 )
 
 FUND_STRUCTURE=(
-    (0,'Open-End'),
-    (1,'Closed-End')
+    (0,'open-ended'),
+    (1,'close-ended')
 )
 
 FUND_STATUS=(
-    (0,'Onboarding'),
-    (1,'Open'),
-    (2,'Funded'),
-    (3,'Re-fund'),
-    (4,'Frozen'),
-    (5,'Unfrozen'),
-    (6,'Close'),
-    (7,'Extend Term')
+    (0,'onboarding'),
+    (1,'open'),
+    (2,'funded'),
+    (3,'refund'),
+    (4,'frozen'),
+    (5,'unfrozen'),
+    (6,'close'),
+    (7,'extendterm')
 )
 
 FUND_YEAR_END_MONTH=(
@@ -47,13 +46,21 @@ FUND_YEAR_END_MONTH=(
     (11,'Dec'),
 )
 
+FUND_APPROVAL_STATUS=(
+    (1,'Pending'),
+    (2,'Approved'),
+    (3,'Rejected')
+)
+
 class User(AbstractUser):
     user_type=models.IntegerField(choices=USER_DESIGNATION,default=4,verbose_name=_("User Designation"),
               help_text="This field determines the designation of the user")
 
-
 class FundCountry(models.Model):
     country_name=models.CharField(max_length=256,blank=False,null=False)
+
+    def __str__(self):
+        return self.country_name
 
 class Director(models.Model):
     director_name=models.CharField(max_length=256)
@@ -62,110 +69,131 @@ class Director(models.Model):
     def __str__(self):
         return self.director_name
 
-# class Subscriber(models.Model):
-#     subscriber_name=models.CharField(max_length=256)
-#     subscriber_commitment=models.DecimalField(decimal_places=2,default=0.00)
-
-
 class ReportingCurrency(models.Model):
     currency=models.CharField(max_length=256,unique=True,null=False)
+
+    def __str__(self):
+        return self.currency
+
 
 class ProductType(models.Model):
     product_type_name=models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.product_type_name
+
+
 class ReportingFrequency(models.Model):
     reporting_frequency_name=models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.reporting_frequency_name
+
+
 
 class ReclassificationFrequency(models.Model):
     reclassification_frequency_name=models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.reclassification_frequency_name
+
 class Bank(models.Model):
     Bank_name=models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.Bank_name
 
 
-
-# class Fund(models.Model):
-#     '''
-#     things to do
-#     1) Director
-
-#     '''
-#     fund_name=models.CharField(max_length=256,blank=False,null=False)
-#     registration_no=models.CharField(max_length=50,blank=False,null=False)
-#     fund_description=models.TextField(blank=False,null=False)
-#     sub_fund=models.ForeignKey('self',null=True,on_delete=models.SET_NULL)
-#     domicile=models.ForeignKey(FundCountry,null=False)
-#     fund_type=models.IntegerField(choices=FUND_TYPE,default=0)
-#     fund_manager_entity=models.CharField(max_length=256,null=True,blank=True)
-#     fund_manager_rep=models.ForeignKey(Director,null=True,on_delete=models.SET_NULL)
-#     fund_structure=models.IntegerField(choices=FUND_STRUCTURE,default=0)
-#     offer_price=models.DecimalField(decimal_places=2,default=1.00)
-#     issued_shares=models.IntegerField(default=1)
-#     ordinary_shares=models.IntegerField(default=1)
-#     fund_status=models.IntegerField(choices=FUND_STATUS,default=0)
-#     report_currency=models.ForeignKey(ReportingCurrency,null=False,on_delete=models.SET_NULL)
-#     fund_size=models.DecimalField(decimal_places=2,default=0.00)
-#     lock_up_period=models.IntegerField(default=0,help_text=_('Time in years'))
-#     fund_year_end=models.IntegerField(choices=FUND_YEAR_END_MONTH,default=11)
-#     product_type=models.ForeignKey(ProductType,on_delete=models.SET_NULL)
-#     # fund_life='done'
-#     fund_end_date=models.DateField()
-#     catch_up=models.DecimalField(decimal_places=2,default=0.00)
-#     reporting_frequency=models.ForeignKey(ReportingFrequency,on_delete=models.SET_NULL)
+class Fund(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    fund_name=models.CharField(max_length=256,blank=False,null=False)
+    registration_no=models.CharField(max_length=50,blank=False,null=False)
+    fund_description=models.TextField(blank=False,null=False)
+    supervisor_approval=models.IntegerField(choices=FUND_APPROVAL_STATUS,default=1)
+    supervisor_reason=models.TextField(blank=True)
+    manager_approval=models.IntegerField(choices=FUND_APPROVAL_STATUS,default=1)
+    manager_reason=models.TextField(blank=True)
+    sub_fund=models.ForeignKey('self',null=True,on_delete=models.SET_NULL,related_name='subfund',blank=True)
+    domicile=models.ForeignKey(FundCountry,null=True,on_delete=models.SET_NULL)
+    fund_type=models.IntegerField(choices=FUND_TYPE,default=0)
+    fund_manager_entity=models.CharField(max_length=256,blank=True)
+    fund_manager_rep=models.ForeignKey(Director,related_name='fund_manager_representator',null=True,on_delete=models.SET_NULL)
+    fund_structure=models.IntegerField(choices=FUND_STRUCTURE,default=0)
+    offer_price=models.DecimalField(max_digits=6,decimal_places=2,default=1.00)
+    issued_shares=models.IntegerField(default=1)
+    ordinary_shares=models.IntegerField(default=1)
+    fund_status=models.IntegerField(choices=FUND_STATUS,default=0)
+    reason_to_change=models.TextField(blank=True)
+    report_currency=models.ForeignKey(ReportingCurrency,null=True,on_delete=models.SET_NULL)
+    fund_size=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    lock_up_period=models.IntegerField(default=0,help_text=_('Time in years'))
+    fund_year_end=models.IntegerField(choices=FUND_YEAR_END_MONTH,default=11)
+    product_type=models.ForeignKey(ProductType,on_delete=models.SET_NULL,null=True)
+    # fund_life='done'
+    fund_end_date=models.DateField()
+    catch_up=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    reporting_frequency=models.ForeignKey(ReportingFrequency,on_delete=models.SET_NULL,null=True,blank=True)
     
-#     legal_counsel=models.CharField(max_length=256,blank=True)
-#     legal_counsel_rep=models.CharField(max_length=256,blank=True)
-#     Auditor=models.CharField(max_length=256,blank=True)
-#     Auditor_rep=models.CharField(max_length=256,blank=True)
-#     Custodian=models.CharField(max_length=256,blank=True)
-#     Custodian_rep=models.CharField(max_length=256,blank=True)
-#     investment_committee=models.ForeignKey(Director,null=False,on_delete=models.SET_NULL)
+    legal_counsel=models.CharField(max_length=256,blank=True)
+    legal_counsel_rep=models.CharField(max_length=256,blank=True)
+    Auditor=models.CharField(max_length=256,blank=True)
+    Auditor_rep=models.CharField(max_length=256,blank=True)
+    Custodian=models.CharField(max_length=256,blank=True)
+    Custodian_rep=models.CharField(max_length=256,blank=True)
+    investment_committee=models.ManyToManyField(Director,related_name='investment_committ')
+    asset_under_management=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    directors=models.ManyToManyField(Director,related_name='directors_all')
+    AuthorizedSignatory=models.ForeignKey(Director,null=True,on_delete=models.SET_NULL,related_name='authorized_sign')
 
-#     asset_under_management=models.DecimalField(decimal_places=2,default=0.00)
-#     directors=models.ManyToManyField(Director)
-#     subscribers=models.ManyToManyField(Subscriber)
-#     AuthorizedSignatory=models.ForeignKey(Director,null=False,on_delete=models.SET_NULL)
-#     BoardResolutions=models.FileField(upload_to='',null=True,blank=True)
-#     FundAdministrator=models.CharField(max_length=256,blank=False,null=False)
-#     GIIN=models.CharField(max_length=50,blank=False,null=False)
-#     Preparer=models.CharField(max_length=256,blank=True,null=True)
-#     # ClosingPeriod=Done
-#     ReclassificationFrequency=models.ForeignKey(ReclassificationFrequency,on_delete=models.SET_NULL)
-#     Approver=models.ForeignKey(Director,null=False,on_delete=models.SET_NULL)
-#     Subscription_Agreement=models.FileField(upload_to='',null=False,blank=False)
-#     Investment_Agreement=models.FileField(upload_to='',null=False,blank=False)
-#     PPM=models.FileField(upload_to='',null=False,blank=False)
-#     Director_Fees=models.DecimalField(decimal_places=2,default=0.00)
-#     Management_Fee=models.DecimalField(decimal_places=2,default=0.00)
-#     Hurdle_Rate=models.DecimalField(decimal_places=4,default=0.0000)
-#     CTC=models.DecimalField(decimal_places=2,default=0.00)
-#     Bank=models.ForeignKey(Bank)
-#     BankAccount=models.PositiveIntegerField(max_length=50,null=False,blank=False)
-#     BankAccessID=models.CharField(max_length=50,blank=False,null=False)
-#     BankAccessPassword=models.CharField(max_length=50,null=False,blank=False)
-
-    
-# class FundLife_Close(models.Model):
-#     fund=models.OneToOneField(Fund,on_delete=models.CASCADE)
-#     fundlife=models.PositiveIntegerField()
-
-# class FundLife_Open(models.Model):
-#     fund=models.OneToOneField(Fund,on_delete=models.CASCADE)
-#     fundlife=models.PositiveIntegerField()
-#     Board_Extension=models.PositiveIntegerField()
-#     Investor_Extension=models.PositiveIntegerField()
-#     # Documents=''
+    FundAdministrator=models.CharField(max_length=256,blank=False,null=False)
+    GIIN=models.CharField(max_length=50,blank=False,null=False)
+    Preparer=models.CharField(max_length=256,blank=True,null=True)
+    ReclassificationFrequency=models.ForeignKey(ReclassificationFrequency,null=True,on_delete=models.SET_NULL)
+    Approver=models.ForeignKey(Director,null=True,on_delete=models.SET_NULL,related_name='approve')
+    Subscription_Agreement=models.FileField(upload_to='subscription_agreement/',null=False,blank=False)
+    Investment_Agreement=models.FileField(upload_to='investment_agreement/',null=False,blank=False)
+    PPM=models.FileField(upload_to='ppm/',null=False,blank=False)
+    Director_Fees=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    Management_Fee=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    Hurdle_Rate=models.DecimalField(max_digits=6,decimal_places=4,default=0.0000)
+    CTC=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
+    Bank=models.ForeignKey(Bank,on_delete=models.SET_NULL,null=True)
+    BankAccount=models.PositiveIntegerField(null=False,blank=False)
+    BankAccessID=models.CharField(max_length=50,blank=False,null=False)
+    BankAccessPassword=models.CharField(max_length=50,null=False,blank=False)
+    redeem=models.CharField(max_length=256,blank=True)
+    redeemReason=models.TextField(blank=True)
+    liquidate=models.CharField(max_length=256,blank=True)
+    liquidateReason=models.TextField(blank=True)
 
     
-# class closingperiod(models.Model):
-#     fund=models.ForeignKey(Fund,on_delete=models.CASCADE)
-#     closing_Date=models.DateField(blank=False,null=False)
+class FundLifeClose(models.Model):
+    fund=models.OneToOneField(Fund,on_delete=models.CASCADE)
+    fundlife=models.PositiveIntegerField()
 
+class FundLifeOpen(models.Model):
+    fund=models.OneToOneField(Fund,on_delete=models.CASCADE)
+    fundlife=models.PositiveIntegerField()
+    Board_Extension=models.PositiveIntegerField()
+    Investor_Extension=models.PositiveIntegerField()
+    # Documents='done'
 
+# class FundLifeOpenDocument(models.Model):
+#     fundlifeopen=models.ForeignKey(FundLifeOpen,on_delete=models.CASCADE)
+#     document_name=models.CharField(max_length=256)
+#     upload_date=models.DateField(auto_now_add=True)
+#     description=models.TextField(blank=True)
+#     document=models.FileField(upload_to='fundlife_opendocument/',null=True,blank=True)
 
+class closingperiod(models.Model):
+    fund=models.ForeignKey(Fund,on_delete=models.CASCADE)
+    closing_Date=models.DateField(blank=False,null=False)
 
+class Subscriber(models.Model):
+    fund=models.ForeignKey(Fund,on_delete=models.CASCADE)
+    subscriber_name=models.CharField(max_length=256)
+    subscriber_commitment=models.DecimalField(max_digits=6,decimal_places=2,default=0.00)
 
-
-
-
+class BoardResolution(models.Model):
+    fund=models.ForeignKey(Fund,on_delete=models.CASCADE)
+    board_resolution=models.FileField(upload_to='board_resolution/',null=True,blank=True)
--- API/core/views.py
+++ API/core/views.py
@@ -1,18 +1,158 @@
-from rest_framework import authentication
 from rest_framework.authtoken.views import ObtainAuthToken
-
 from core.models import Director
-from .serializers import AuthTokenSerializer, DirectorSerializer
+from .serializers import AuthTokenSerializer, DirectorSerializer,FundSerializer
 from rest_framework.settings import api_settings
 from rest_framework.decorators import api_view, authentication_classes,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.views import APIView
-from rest_framework import status
 from django.contrib.auth import logout
+import json
+from .models import (BoardResolution, Fund,FundCountry,Director,FUND_TYPE,FUND_STRUCTURE,
+                    FUND_STATUS,FUND_YEAR_END_MONTH,ReportingCurrency,ProductType,
+                    ReportingFrequency,ReclassificationFrequency,Bank,FundLifeClose,
+                    FundLifeOpen,closingperiod,Subscriber)
+                
+from .utils import get_id_of_tuple,get_object_or_none
 
 # Create your views here.
+
+def create_fund_object(data,request,sub_fund=False):
+    domicile_obj=get_object_or_none(FundCountry,'domicile',data['domicile'])
+    report_currency=get_object_or_none(ReportingCurrency,'report_currency',data['reportingCurrency'])
+    product_type=get_object_or_none(ProductType,'product_type',data['productType'])
+    reporting_frequency=get_object_or_none(ReportingFrequency,'reporting_frequency',data['reportingFrequency'])
+    AuthorizedSignatory=get_object_or_none(Director,'None',data['authorizedSignatory'][0])
+    fundManagerRep=get_object_or_none(Director,'None',data['fundManagerRep'][0])
+
+    if AuthorizedSignatory!=None and AuthorizedSignatory.director_signature is None:
+        AuthorizedSignatory.director_signature=request.FILES.get('signature' if not sub_fund else 's_signature',None)
+        AuthorizedSignatory.save()
+
+    ReclassificationFrequency_obj=get_object_or_none(ReclassificationFrequency,'ReclassificationFrequency',data['reclassificationFreq'])
+    Bank_obj=get_object_or_none(Bank,'Bank',data['bank'])
+    Approver=get_object_or_none(Director,'None',data['approver'][0])
+
+    fund_obj=Fund(user=request.user,
+                fund_name=data['fundName'],
+                registration_no=data['registrationNumber'],
+                fund_description=data['fundDescription'],
+                sub_fund=None,
+                domicile=domicile_obj,
+                fund_type=get_id_of_tuple(data['fundType'],FUND_TYPE),
+                fund_structure=get_id_of_tuple(data['fundStructure'],FUND_STRUCTURE),
+                fund_manager_entity=data['fundManagerEntity'],
+                fund_manager_rep=fundManagerRep,
+                offer_price=float(data['offerPrice']),
+                issued_shares=int(data['issuedShares']),
+                ordinary_shares=int(data['ordinaryShare']),
+                fund_status=get_id_of_tuple(data['fundStatus'],FUND_STATUS),
+                reason_to_change='',
+                report_currency=report_currency,
+                fund_size=float(data['fundSize']),
+                lock_up_period=int(data['lockupPeriod']),
+                fund_year_end=get_id_of_tuple(data['fundYearEnd'],FUND_YEAR_END_MONTH),
+                product_type=product_type,
+                fund_end_date=data['fundEndDate'],
+                catch_up=float(data['catchup']),
+                reporting_frequency=reporting_frequency,
+                legal_counsel=data['legalCounsel'],
+                legal_counsel_rep=data['legalCounselRep'],
+                Auditor=data['auditor'],
+                Auditor_rep=data['auditorRep'],
+                Custodian=data['trustee'],
+                Custodian_rep=data['trusteeRep'],
+                asset_under_management=float(data['assetUnderManagement']),
+                AuthorizedSignatory=AuthorizedSignatory,
+                FundAdministrator=data['fundAdmin'],
+                GIIN=data['GIIN'],
+                Preparer=data['preparer'],
+                ReclassificationFrequency=ReclassificationFrequency_obj,
+                Approver=Approver,
+                Subscription_Agreement=request.FILES.get('subscriptionAgreement' if not sub_fund else 's_subscriptionAgreement',None),
+                Investment_Agreement=request.FILES.get('investmentAgreement' if not sub_fund else 's_investmentAgreement',None),
+                PPM=request.FILES.get('PPM' if not sub_fund else 's_PPM',None),
+                Director_Fees=float(data['directorFee']),
+                Management_Fee=float(data['managementFee']),
+                Hurdle_Rate=float(data['hurdleRate']),
+                CTC=float(data['CTC']),
+                Bank=Bank_obj,
+                BankAccount=data['bankAccount'],
+                BankAccessID=data['bankAccessId'],
+                BankAccessPassword=data['bankAccessPassword'],
+                redeem=data['redeem'],
+                redeemReason=data['redeemReason'],
+                liquidate=data['liquidate'],
+                liquidateReason=data['liquidateReason']
+            )
+    fund_obj.save()
+
+    #Adding Directors to fund
+    if len(data['directors'])>0:
+        
+        for director_pk in data['directors']:
+            obj=Director.objects.get(pk=director_pk)
+            if 'director_{}'.format(director_pk) in request.FILES:
+                obj.director_signature=request.FILES['director_{}'.format(director_pk)]
+                obj.save()
+
+            fund_obj.directors.add(obj)
+        fund_obj.save()
+    
+    # #adding fund_manager_entity 
+    # if(len(data['fundManagerEntity'])>0):
+    #     for director_pk in data['fundManagerEntity']:
+    #         obj=Director.objects.get(pk=director_pk)
+    #         fund_obj.fund_manager_entity.add(obj)
+    #     fund_obj.save()
+    
+    # #adding fund_manager_rep    
+    # if(len(data['fundManagerRep'])>0):
+    #     for director_pk in data['fundManagerRep']:
+    #         obj=Director.objects.get(pk=director_pk)
+    #         fund_obj.fund_manager_rep.add(obj)
+    #     fund_obj.save()
+    
+    #adding investment committee
+    if(len(data['investmentComittee'])>0):
+        for director_pk in data['investmentComittee']:
+            obj=Director.objects.get(pk=director_pk)
+            fund_obj.investment_committee.add(obj)
+        fund_obj.save()
+
+
+    #open close table creation
+    if data['fundStructure']=='open-ended':
+        obj=FundLifeOpen(fund=fund_obj,fundlife=int(data['fundLifeYears']),
+                        #below fields not given from frontend assumed!
+                        Board_Extension=int(data['boardExtension']),Investor_Extension=int(data['investorExtension']))
+        obj.save()
+    else:
+        obj=FundLifeClose(fund=fund_obj,fundlife=int(data['fundLifeYears']))
+        obj.save()
+    #end
+    
+    #closingPeriod object creation
+    for date in data['closingPeriod']:
+        obj=closingperiod(fund=fund_obj,closing_Date=date)
+        obj.save()
+    #end
+
+    #subscriber creation
+    if(len(data['subscribers'])!=0):
+        for obj in data['subscribers']:
+            obj=Subscriber(fund=fund_obj,subscriber_name=obj['name'],subscriber_commitment=float(obj['commitment']))
+            obj.save()
+    #end
+
+    #boardresolution creation
+    if 'boardResolutions' or 's_boardResolutions' in request.FILES:
+        for file in request.FILES.getlist('boardResolutions' if not sub_fund else 's_boardResolutions' ):
+            obj=BoardResolution(fund=fund_obj,board_resolution=file)
+            obj.save()
+    
+    return fund_obj
+
 
 class UserLoginView(ObtainAuthToken):
     serializer_class=AuthTokenSerializer
@@ -44,16 +184,99 @@ def Director_API(request):
         Directors=Director.objects.all()
         return Response(DirectorSerializer(Directors,many=True).data)
     elif request.method=='POST':
-        name=request.POST.get('name')
-        Director_object=Director(director_name=name)
-        Director_object.save()
+        for directors in request.POST.getlist('name'):
+            Director_object=Director(director_name=directors)
+            Director_object.save()
         return Response(DirectorSerializer(Director_object).data)
     elif request.method=='DELETE':
         id=request.POST.get('id')
         Director.objects.get(pk=int(id)).delete()
         return Response('Director Deleted Successfully')
 
+@api_view(["POST",])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication,])
+def create_fund_view(request):
+    '''
+      fundName: ['', [Validators.required, Validators.maxLength(5)]],
+      registrationNumber: ['', [Validators.required, Validators.maxLength(50)]],
+      fundDescription: ['', [Validators.required, Validators.maxLength(2048)]],
+      subFund: ['N', []],
+      domicile: ['singapore', []],
+      fundType: ['regulated', []],
+      fundManagerEntity: ['', []],
+      fundManagerRep: ['', [Validators.required]],
+      fundStructure: ['open-end', []],
+      offerPrice: [1.0, []],
+      fundSize: [0.0, []],
+      issuedShares: [1, []],
+      ordinaryShare: [1, []],
+      fundStatus: ['onboarding', []],
+      reportingCurrency: ['USD', []],
+      lockupPeriod: [0, []],
+      fundYearEnd: ['Dec', []],
+      productType: ['private-equity', []],
+      fundLife: [null, [Validators.required]],
+      fundEndDate: [null, []],
+      catchup: [0.0, []],
 
+      // Section 2:
+      reportingFrequency: ['month', []],
+      legalCounsel: ['', []],
+      legalCounselRep: ['', []],
+      auditor: ['', []],
+      auditorRep: ['', []],
+      trustee: ['', []],
+      trusteeRep: ['', []],
+      investmentComittee: [new FormArray([]), [Validators.required]], 
+      directors: [new FormArray([]), [Validators.required]],    to discuss
+      directorSignature: ['', [Validators.required]],        
+      subscribers: [new FormArray([]), [Validators.required]],
+      subscribersCommitment: [0.0, [Validators.required]],      
 
+      // Section 3:
+      authorizedSignatory: [[], [Validators.required]],
+      signature: [null, [Validators.required]], -- add in signature endpoint
+      boardResolutions: [null, [Validators.required]],
+      fundAdmin: ['', [Validators.required]],
+      GIIN: ['', [Validators.required]],
+      preparer: ['', []],
+      closingPeriod: [[], [Validators.required]],
+      reclassificationFreq: ['month', []],
+      approver: ['', [Validators.required]],
+      subscriptionAgreement: [null, [Validators.required]],
+      investmentAgreement: [null, [Validators.required]],
+      PPM: [null, [Validators.required]],
+      directorFee: [0.0, []],
+      managementFee: [0.0, []],
+      hurdleRate: [0.0, []],
+      CTC: [0.0, []],
+      bank: ['ocbc', []],
+      bankAccount: ['', [Validators.required]],
+      bankAccessId: ['', [Validators.required]],
+      bankAccessPassword: ['', [Validators.required]],
 
+      // Section 4:  -- not defined in model
+      freeze: ['', []],
+      freezeReason: ['', []],
+      unfreeze: ['', []],
+      unfreezeReason: ['', []],
+      refund: [0.0, []],
+      refundReason: ['', []],
+      redeem: ['', []],
+      redeemReason: ['', []],
+      extend: ['', []],
+      extendReason: ['', []],
+      liquidate: ['', []],
+      liquidateReason: ['', []], 
 
+      created_at: ['', []], -- Not defined in model
+      updated_at: ['', []], -- Not defined in model
+    '''
+    data=json.loads(request.POST['json'])
+    fund_obj=create_fund_object(data,request)
+    if data['subFund']!='N':
+        fund_obj.sub_fund=create_fund_object(data['subFundData'],request,sub_fund=True)
+        fund_obj.save()
+        
+    return Response(FundSerializer(fund_obj).data)
